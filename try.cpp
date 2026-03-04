ef558f7ec4f58131252aec2ed79fe9896caedb99
Anish Chowdhury <chowdhury.anish.2006@gmail.com>
2026-03-04 16:17:54 +0530

    string fnx

--- try.cpp
+++ try.cpp
@@ -3,13 +3,11 @@
 using namespace std;
 
 int main(){
-    auto a = 34;
-    a +=6;
-    cout<< a << a++ ;
-
-
-
-
+    string first_name = "anish";
+    string last_name = "chowdhury";
+    string full_name = first_name.append(last_name);
+    cout << full_name << endl;
+    cout << full_name.length() ;
 
     return 0;
 }
