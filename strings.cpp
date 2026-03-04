784193bb51bf91decf98ad58086d7e3f7381101a
Anish Chowdhury <chowdhury.anish.2006@gmail.com>
2026-03-04 18:38:25 +0530

    maupalators

---
+++ strings.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cmath>
+using namespace std;
+int main(){
+    cout << min(5,10) <<endl;
+    cout << sqrt(25)<<endl;
+    cout << round(7.541)<<endl;
+    cout << log(10)<<endl;
+    bool mals = true ;
+    bool nmc_mals = false;
+    cout << boolalpha ;
+    cout << mals <<endl;
+    cout << nmc_mals<<endl;
+    cout << noboolalpha ;
+    cout << mals <<endl;
+    cout << nmc_mals<<endl;
+
+    return 0;
+
+}
--- try.cpp
+++ try.cpp
@@ -7,7 +7,12 @@ int main(){
     string last_name = "chowdhury";
     string full_name = first_name.append(last_name);
     cout << full_name << endl;
-    cout << full_name.length() ;
+    cout << full_name.length()<<endl;
+    cout << full_name.at(6)<< endl;
+    full_name[0] = 'B';
+    cout << full_name << endl;
+    cout << "hello \"h\ti\" \\lkfjslfj";
+
 
     return 0;
 }
